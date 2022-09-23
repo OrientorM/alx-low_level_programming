73b7a39aa906a29bd34ca77b3f1aa7fef11284f8
OrientorM <orientororeneile@gmail.com>
2022-09-22 18:33:49 -0700

    task one

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"                                                  
                                                                    
 /**                                                                
-*reset_to_98 - resets the value of given integer to 98             
-*@n: parameter                                                     
-*Return : nothing                                                  
-*/                                                                 
-                                                                   
-void reset_to_98(int *n)                                           
+ * swap_int - Swaps the values of two integers.                    
+ * @a: The first integer to be swapped.                            
+ * @b: The second integer to be swapped.                           
+ */                                                                
+void swap_int(int *a, int *b)                                      
 {                                                                  
-        *n = 98;                                                   
+        int tmp = *a;                                              
+        *a = *b;                                                   
+        *b = tmp;                                                  
 }
